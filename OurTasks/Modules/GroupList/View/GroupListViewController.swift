016abf11d343b31036da46ea62ec5132e06af0a8
Jakub Nykiel <j.nykiel93@gmail.com>
2017-10-31 14:11:18 +0100

    changes in project structure

---
+++ OurTasks/Modules/GroupList/View/GroupListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GroupListViewController.swift
+//  OurTasks
+//
+//  Created by Jakub Nykiel on 03/10/2017.
+//  Copyright © 2017 Jakub Nykiel. All rights reserved.
+//
+
+import UIKit
+
+class GroupListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
