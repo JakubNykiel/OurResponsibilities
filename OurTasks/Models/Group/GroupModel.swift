c3201e88139e56fa29e94654b450a0004bec439e
Jakub Nykiel <j.nykiel93@gmail.com>
2017-11-20 21:58:49 +0100

    group model

---
+++ OurTasks/Models/Group/GroupModel.swift
@@ -0,0 +1,36 @@
+//
+//  GroupModel.swift
+//  OurTasks
+//
+//  Created by Jakub Nykiel on 20.11.2017.
+//  Copyright © 2017 Jakub Nykiel. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class GroupModel: Mappable {
+    var name: String!
+    var createDate: String!
+    var users: [String]!
+    var tasks: [String]!
+    var admins: [String]!
+    var userInteraction: Bool!
+    
+    init() {
+        
+    }
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        self.name <- map["name"]
+        self.createDate <- map["createDate"]
+        self.users <- map["users"]
+        self.tasks <- map["tasks"]
+        self.admins <- map["admins"]
+        self.userInteraction <- map["userInteraction"]
+    }
+}
