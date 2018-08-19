3d03a7c520480529ea6037a5175eac64500f470b
Jakub Nykiel <j.nykiel93@gmail.com>
2018-08-20 01:05:59 +0200

    Event view model without rxdatasources

--- OurTasks/Modules/Event/Event/Logic/EventViewModel.swift
+++ OurTasks/Modules/Event/Event/Logic/EventViewModel.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import RxSwift
+import Firebase
+import CodableFirebase
+
+class EventViewModel {
+    
+    var myTasksBehaviorSubject: BehaviorSubject<[String:TaskModel]> = BehaviorSubject(value: [:])
+    var doneTasksBehaviorSubject: BehaviorSubject<[String:TaskModel]> = BehaviorSubject(value: [:])
+    var eventUsersBehaviorSubject: BehaviorSubject<[String:UserModel]> = BehaviorSubject(value: [:])
+    var noTasksBehaviorSubject: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    var sectionsBehaviourSubject: BehaviorSubject<[EventSection]> = BehaviorSubject(value: [])
+    
+    private let firebaseManager: FirebaseManager = FirebaseManager()
+    private let disposeBag = DisposeBag()
+    private var sections: [EventSection] = []
+    
+    var eventTasks: [String:TaskModel] = [:]
+    var eventModel: EventModel
+    var eventID: String
+    
+    init(eventModel: EventModel, eventID: String) {
+        self.eventID = eventID
+        self.eventModel = eventModel
+    }
+}
